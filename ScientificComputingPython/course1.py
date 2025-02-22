c6565a06e0508ef7897c10beb0565752787c505e
Tino Dietel <tino.dietel@gmail.com>
2025-02-22 06:44:29 +0100

    py course 1

---
+++ ScientificComputingPython/course1.py
@@ -0,0 +1,46 @@
+# Learn String Manipulation by Building a Cipher
+
+def vigenere(message, key, direction=1):
+    """
+    Encrypts or decrypts a message using the Vigenère cipher.
+
+    Parameters:
+    - message (str): The message to be encrypted or decrypted.
+    - key (str): The encryption/decryption key.
+    - direction (int, optional): The direction of the cipher. Use 1 for encryption and -1 for decryption. Default is 1.
+
+    Returns:
+    - str: The encrypted or decrypted message.
+    """
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    final_message = ''
+
+    for char in message.lower():
+
+        # Append any non-letter character to the message
+        if not char.isalpha():
+            final_message += char
+        else:        
+            # Find the right key character to encode/decode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted/decrypted letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    
+    return final_message
+
+def encrypt(message, key):
+    return vigenere(message, key)
+    
+def decrypt(message, key):
+    return vigenere(message, key, -1)
+
+print(f'\nEncrypted text: {text}')
+print(f'Key: {custom_key}')
+decryption = decrypt(text, custom_key)
+print(f'\nDecrypted text: {decryption}\n')
