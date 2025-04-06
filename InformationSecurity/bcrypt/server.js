c6883b48343108ea1ba9afc3bc0e6b00daa093d0
Tino Dietel <tino.dietel@gmail.com>
2025-04-06 16:50:57 +0200

    add bycrypt example

---
+++ InformationSecurity/bcrypt/server.js
@@ -0,0 +1,36 @@
+'use strict';
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const app         = express();
+fccTesting(app);
+const saltRounds = 12;
+const myPlaintextPassword = 'sUperpassw0rd!';
+const someOtherPlaintextPassword = 'pass123';
+
+const bcrypt = require('bcrypt')
+
+//START_ASYNC -do not remove notes, place code between correct pair of notes.
+
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res); //true
+    });
+});
+
+//END_ASYNC
+
+//START_SYNC
+
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
+
+//END_SYNC
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Listening on port:", PORT)
+});
